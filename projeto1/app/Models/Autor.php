e62f71cfa24e0e5e09feb58e717d3b91f418c6da
Eduardo da Silva Paim <eduardo-paim@SEHAB.INTRA.RS.GOV.BR>
2025-12-11 17:25:59 -0300

    add Autor model with UUID generation and fillable attributes

--- projeto1/app/Models/Autor.php
+++ projeto1/app/Models/Autor.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Autor extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {                
+                $model->{$model->getKeyName()} = (string) Str::uuid();            
+            }
+        });
+    }
+
+    protected $fillable = [
+        'nome',
+        'data_nascimento',
+        'nacionalidade',
+        'biografia',
+    ];
+
+    public function livros()
+    {
+        return $this->hasMany(Livro::class);
+    }
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
 }
